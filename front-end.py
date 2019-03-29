80b082dd91a416607628e88e95e6bf7a428ed8f8
Shoaib Khan <shoaiba.khan@mail.utoronto.ca>
2019-03-29 14:28:56 -0400

    Backend complete, Everything working in testing

--- front-end.py
+++ front-end.py
@@ -1,6 +1,10 @@
 from tkinter import *
 from password_generator import *
 
+lower_checked = 0
+upper_checked = 0
+num_checked = 0
+spec_checked = 0
 
 def info_popup():
     win = Toplevel()
@@ -9,8 +13,7 @@ def info_popup():
     l = Label(win, text="In order to generate a unique and secure password, please enter your username, and master-password. \n Tips on creating a secure password: \n 1. Choose the max-length of characters allowed \n 2. Select all the special password options that the website allows (ie: special characters, capitals, numbers, etc.) ", fg="#00FF00",bg='Black')
     l.grid(row=0, column=0)
 
-    close = Button(win, text="Close", command=win.destroy,
-                   bg="Red", fg="Black")
+    close = Button(win, text="Close", command=win.destroy,bg="Red", fg="Black")
     close.grid(row=1, column=0)
 
     win.configure(background='Black')
@@ -25,46 +28,45 @@ def set_text(entry, text):
 
 
 def display_passowrd(event):
-
-    alphabet = ""
     if lower_state.get():
-        alphabet += "abcdefghijklmnopqrstuvwxyz"
+        lower_checked = 1
+    else:
+        lower_checked = 0
     if upper_state.get():
-        alphabet += "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        upper_checked = 1
+    else:
+        upper_checked = 0
     if nums_state.get():
-        alphabet += "0123456789"
+        num_checked = 1
+    else:
+        num_checked = 0
     if special_char_state.get():
-        alphabet += "!@#$%^&*()-_"
+        spec_checked = 1
+    else:
+        spec_checked = 0
 
     website = entry_website.get()
     user_name = entry_user.get()
     master_password = entry_master_pass.get()
     length = entry_passwrd_len.get()
+    length = int(length)
 
-    if not (website and user_name and master_password and alphabet and length):
+    if not (website and user_name and master_password and (length >= 8 and length <= 20) and (lower_checked or upper_checked or num_checked or spec_checked)):
         return
 
-    length = int(length)
-    password = generate_password(
-        website, user_name, master_password, length, alphabet)
+    password = generate_password(website, user_name, master_password, length, lower_checked, upper_checked, num_checked, spec_checked)
     set_text(psswrd_generated, password)
     print("Secure Password Generated!")
 
 
 root = Tk()
 
-alphabet = []
+lower_state, upper_state, nums_state, special_char_state = IntVar(value=1), IntVar(value=1), IntVar(value=1), IntVar(value=1)
 
-lower_state, upper_state, nums_state, special_char_state = IntVar(
-    value=1), IntVar(value=1), IntVar(value=1), IntVar(value=1)
-
-lower = Checkbutton(root, text="lowercase", fg="#00FF00",bg="Black", variable=lower_state)
-upper = Checkbutton(root, text="UPPERCASE", fg="#00FF00",
-                    bg="Black", variable=upper_state)
-nums = Checkbutton(root, text="Numbers", fg="#00FF00",
-                   bg="Black", variable=nums_state)
-special_char = Checkbutton(
-    root, text="Special Characters", fg="#00FF00", bg="Black", variable=special_char_state)
+lower = Checkbutton(root, text="Lowercase", fg="#00FF00",bg="Black", variable=lower_state)
+upper = Checkbutton(root, text="Uppercase", fg="#00FF00",bg="Black", variable=upper_state)
+nums = Checkbutton(root, text="Numbers", fg="#00FF00",bg="Black", variable=nums_state)
+special_char = Checkbutton(root, text="Special Characters", fg="#00FF00", bg="Black", variable=special_char_state)
 
 
 lower.grid(row=6, column=0)
@@ -94,19 +96,16 @@ entry_passwrd_len = Entry(root)
 label_passwrd_len.grid(row=4, column=0)
 entry_passwrd_len.grid(row=4, column=1)
 
-button_generate = Button(root, text="Generate", height=3,
-                         width=15, bg="Red", fg="Black")
+button_generate = Button(root, text="Generate", height=3,width=15, bg="Red", fg="Black")
 psswrd_generated = Text(root, state=DISABLED, height=5, width=16)
 
 button_generate.bind("<Button-1>", display_passowrd)
-
 
 menubar = Menu(root)
 filemenu = Menu(menubar, tearoff=0)
 filemenu.add_command(label="More Info", command=info_popup)
 menubar.add_cascade(label="Help", menu=filemenu)
 root.config(menu=menubar)
-
 
 button_generate.grid(row=5, column=0)
 psswrd_generated.grid(row=5, column=1)
--- password_generator.py
+++ password_generator.py
@@ -1,6 +1,11 @@
 import hashlib
 import binascii
+import re
 
+lower_alpha = "abcdefghijklmnopqrstuvwxyz"
+upper_alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+num_alpha = "0123456789"
+spec_alpha = "!@#$%^&*()-_"
 
 def generate_salt(website, user_name):
     return website + user_name
@@ -36,7 +41,32 @@ def decode_utf(txt):
     return txt.decode("utf-8", 'ignore')
 
 
-def generate_password(website, user_name, master_password, length, alphabet):
+def generate_password(website, user_name, master_password, length, lower_state, upper_state, nums_state, special_char_state):
+
+    #Section below determines the characters used in the password
+    alphabet = ""
+    if lower_state == 1:
+        alphabet += lower_alpha
+        low_met = 0
+    else:
+        low_met = 1
+    if upper_state == 1:
+        alphabet += upper_alpha
+        upp_met = 0
+    else:
+        upp_met = 1
+    if nums_state == 1:
+        alphabet += num_alpha
+        num_met = 0
+    else:
+        num_met = 1
+    if special_char_state == 1:
+        alphabet += spec_alpha
+        spec_met = 0
+    else:
+        spec_met = 1
+
+    #Section below actually creates the password    
     salted = generate_salt(website, user_name)
     peppered = generate_pepper(website, master_password)
     middle = length
@@ -45,6 +75,25 @@ def generate_password(website, user_name, master_password, length, alphabet):
     ver_two = ver_two[:middle]
     ver_two = Decimal_to_hexdecimal(ver_two)
     ver_three = decimal_to_alphabet(ver_two, length, alphabet)
+
+    #Section below makes sure each password_requirement is met
+    if low_met == 0 and not(re.search('[a-z]', ver_three)):
+        print("lower entered")
+        ver_three = ver_three[:-1] + lower_alpha[int(length)%27]
+    low_met = 1
+    if upp_met == 0 and not(re.search('[A-Z]', ver_three)):
+        ver_three = ver_three[:-2] + upper_alpha[int(length)%27] + ver_three[-1:]
+        print("upper entered")
+    upp_met = 1
+    if num_met == 0 and not(re.search('[0-9]', ver_three)):
+        ver_three = ver_three[:-3] + num_alpha[int(length)%10] + ver_three[-2:]
+        print("nums entered")
+    num_met = 1
+    if spec_met == 0 and not(re.search('[!@#$%^&*()\-_]', ver_three)):
+        ver_three = ver_three[:-4] + spec_alpha[int(length)%12] + ver_three[-3:]
+        print("spec entered")
+    spec_met = 1
+            
     return ver_three
 
 
